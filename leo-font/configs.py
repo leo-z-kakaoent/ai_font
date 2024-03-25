49ccd5ae7ac91d7a9eb878a4d58f3c4347b7466d
Leo <leo.z@kakaoent.com>
2024-03-25 16:14:22 +0900

    minor

--- leo-font/configs.py
+++ leo-font/configs.py
@@ -1,7 +1,7 @@
 class DefaultConfig:
     def __init__(self):
         self.lr = 0.0001
-        self.dataset_path = "../data/raw_128/"
+        self.dataset_path = "../data/raw_png_128/"
         self.batch_size = 64
         self.G_ch = 64
         self.n_embedding = 1024
@@ -10,6 +10,6 @@ class DefaultConfig:
 class SCRConfig(DefaultConfig):
     def __init__(self):
         super().__init__()
-        self.scr_dataset_path = "../data/raw_96/"
+        self.scr_dataset_path = "../data/raw_png_96/"
         self.scr_model_path = "../data/model/scr_98.pth"
         self.scr_coef = 0.01
