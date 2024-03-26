f7d87c195966de50a0d66ec50ae810d72c6fbf66
Leo <leo.z@kakaoent.com>
2024-03-26 01:53:20 +0000

    working

--- leo-font/utils.py
+++ leo-font/utils.py
@@ -1,17 +1,29 @@
 import torch
+import numpy as np
 from datetime import datetime
 from PIL import Image
+
+def tensor2img(img_tensor):
+    imgarray = img_tensor.detach().cpu().numpy().transpose((1,2,0))
+    imgarray = ((imgarray + 1) / 2 *255).astype(np.uint8)
+    return Image.fromarray(imgarray)
+
+def img_concat(img0, img1, r=128):
+    plate = Image.new("RGB", (r*2, r))
+    plate.paste(img0, (0,0))
+    plate.paste(img1, (r,0))
+    return plate
 
 def save_package(img_dict, model_dict, path, prefix):
     current_time = datetime.now().strftime('%Y%m%d%H%M%S')
     for k,v in img_dict.items():
         p = f"{path}/{prefix}__{k}_{current_time}.png"
-        Image.fromarray(v).save(p)
-        print(f"Saved: {p}")
+        img_concat(tensor2img(v[0]),tensor2img(v[1])).save(p)
+        # print(f"Saved: {p}")
     for k,v in model_dict.items():
         p = f"{path}/{prefix}__{k}_{current_time}.pth"
         torch.save(v, p)
-        print(f"Saved: {p}")
+        # print(f"Saved: {p}")
 
 def set_requires_grad(nets, requires_grad=False):
     if not isinstance(nets, list):
--- leo-font/vanilla_train_original.py
+++ leo-font/vanilla_train_original.py
@@ -3,6 +3,7 @@ import itertools
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 import os
+import numpy as np
 
 from dataset import FontDataset
 from models import Discriminator, ContentEncoder, StyleEncoder, Decoder
@@ -22,7 +23,7 @@ save_path = configs.save_path
 
 accelerator = Accelerator()
 dataset = FontDataset(path=dataset_path)
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, drop_last=True, num_workers=6)
 discriminator = Discriminator()
 content_encoder = ContentEncoder(G_ch=g_ch)
 style_encoder = StyleEncoder(G_ch=g_ch)
@@ -39,44 +40,55 @@ criterionD = DisLoss()
 dataloader, content_encoder, style_encoder, decoder, discriminator, criterionD, optimizer_D, optimizer_G = accelerator.prepare(
     dataloader, content_encoder, style_encoder, decoder, discriminator, criterionD, optimizer_D, optimizer_G)
 
-for epoch in tqdm(range(n_epoch)):
-    for data in dataloader:
-        cont, residulte_features = content_encoder(data['content'])
-        style_emb, style_fc, residual_features_style = style_encoder(data['style'])
-        img_print2write = decoder(cont, residulte_features, style_emb, style_fc, residual_features_style)
-        fake_out = discriminator(img_print2write)
+pbar = tqdm(total=n_epoch*1000)
+c = 0
+while c < n_epoch*1000:
+    try:
+        data = next(loader)
+    except:
+        loader = iter(dataloader)
+        data = next(loader)
+        # print("Dataloader Loaded")
+    
+    cont, residulte_features = content_encoder(data['content'])
+    style_emb, style_fc, residual_features_style = style_encoder(data['style'])
+    img_print2write = decoder(cont, residulte_features, style_emb, style_fc, residual_features_style)
+    fake_out = discriminator(img_print2write)
 
-        loss_G = criterionD(fake_out, True)
+    loss_G = criterionD(fake_out, True)
 
-        set_requires_grad([content_encoder, style_encoder, decoder], True)
-        set_requires_grad([discriminator], False)
-        optimizer_G.zero_grad()
-        accelerator.backward(loss_G)
-        optimizer_G.step()
+    set_requires_grad([content_encoder, style_encoder, decoder], True)
+    set_requires_grad([discriminator], False)
+    optimizer_G.zero_grad()
+    accelerator.backward(loss_G)
+    optimizer_G.step()
 
-        cont, residulte_features = content_encoder(data['content'])
-        style_emb, style_fc, residual_features_style = style_encoder(data['style'])
-        img_print2write = decoder(cont, residulte_features, style_emb, style_fc, residual_features_style)
-        fake_out = discriminator(img_print2write)
-        real_out = discriminator(data['target'])
-        loss_D = criterionD(real_out, True) + criterionD(fake_out, False)
+    cont, residulte_features = content_encoder(data['content'])
+    style_emb, style_fc, residual_features_style = style_encoder(data['style'])
+    img_print2write = decoder(cont, residulte_features, style_emb, style_fc, residual_features_style)
+    fake_out = discriminator(img_print2write)
+    real_out = discriminator(data['target'])
+    loss_D = criterionD(real_out, True) + criterionD(fake_out, False)
 
-        set_requires_grad([content_encoder, style_encoder, decoder], False)
-        set_requires_grad([discriminator], True)
-        optimizer_D.zero_grad()
-        accelerator.backward(loss_D)
-        optimizer_D.step()
+    set_requires_grad([content_encoder, style_encoder, decoder], False)
+    set_requires_grad([discriminator], True)
+    optimizer_D.zero_grad()
+    accelerator.backward(loss_D)
+    optimizer_D.step()
+    
+    pbar.update(1)
+    pbar.set_postfix(str=f"loss_G: {loss_G}, loss_D: {loss_D}")
 
-        print(f"loss_G: {loss_G}, loss_D: {loss_D}")
-
-    save_package(
-        img_dict={f"generated{str(i)}": img_print2write[i,:,:,:].detach().cpu().numpy() for i in range(len(img_print2write))},
-        model_dict={
-            "content_encoder": content_encoder.state_dict(),
-            "style_encoder": style_encoder.state_dict(),
-            "decoder": decoder.state_dict(),
-            "discriminator": discriminator.state_dict()
-        },
-        path=save_path,
-        prefix=os.path.basename(__file__).replace(".py", ""),
-    )
+    if c % 100 == 0:
+        save_package(
+            img_dict={"generated_img": (data['target'][0,:,:,:], img_print2write[0,:,:,:])},
+            model_dict={
+                "content_encoder": content_encoder.state_dict(),
+                "style_encoder": style_encoder.state_dict(),
+                "decoder": decoder.state_dict(),
+                "discriminator": discriminator.state_dict()
+            },
+            path=save_path,
+            prefix=os.path.basename(__file__).replace(".py", ""),
+        )
+    c+= 1
