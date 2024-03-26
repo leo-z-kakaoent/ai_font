a9cad2045a9274551996ba547022509fc6499969
Leo <leo.z@kakaoent.com>
2024-03-26 18:07:15 +0900

    scr

--- leo-font/scr_train_target.py
+++ leo-font/scr_train_target.py
@@ -10,7 +10,7 @@ from models import Discriminator, ContentEncoder, StyleEncoder, Decoder, SCR
 from losses import DisLoss
 from accelerate import Accelerator
 from configs import SCRConfig
-from utils import save_package
+from utils import save_package, set_requires_grad
 
 configs = SCRConfig()
 batch_size = configs.batch_size
@@ -44,50 +44,69 @@ optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=lr)
 
 criterionD = DisLoss()
 
-dataloader, content_encoder, style_encoder, decoder, discriminator, optimizer_D, optimizer_G = accelerator.prepare(
-    dataloader, content_encoder, style_encoder, decoder, discriminator, optimizer_D, optimizer_G)
+dataloader, content_encoder, style_encoder, decoder, discriminator, criterionD, optimizer_D, optimizer_G = accelerator.prepare(
+    dataloader, content_encoder, style_encoder, decoder, discriminator, criterionD, optimizer_D, optimizer_G)
 
 scr = scr.to(accelerator.device)
+pbar = tqdm(total=n_epoch * 1000)
+c = 0
+while c < n_epoch * 1000:
+    try:
+        data = next(loader)
+    except:
+        loader = iter(dataloader)
+        data = next(loader)
+        # print("Dataloader Loaded")
 
-for epoch in tqdm(range(n_epoch)):
-    for data in dataloader:
-        optimizer_D.zero_grad()
-        optimizer_G.zero_grad()
+    cont, residulte_features = content_encoder(data['content'])
+    style_emb, style_fc, residual_features_style = style_encoder(data['style'])
+    img_print2write = decoder(cont, residulte_features, style_emb, style_fc, residual_features_style)
+    fake_out = discriminator(img_print2write)
 
-        cont, residulte_features = content_encoder(data['content'])
-        style_emb, style_fc, residual_features_style = style_encoder(data['style'])
-        img_print2write = decoder(cont, residulte_features, style_emb, style_fc, residual_features_style)
+    sample_style_embeddings, pos_style_embeddings, neg_style_embeddings = scr(
+        F.interpolate(img_print2write, size=(96, 96), mode='bilinear', align_corners=False),
+        data['target'],
+        data['negative'],
+        nce_layers='0,1,2,3')
+    sc_loss = scr.calculate_nce_loss(
+        sample_s=sample_style_embeddings,
+        pos_s=pos_style_embeddings,
+        neg_s=neg_style_embeddings)
 
-        real_out = discriminator(data['target'])
-        fake_out = discriminator(img_print2write)
+    loss_G = criterionD(fake_out, True) + sc_loss * scr_coef
 
-        sample_style_embeddings, pos_style_embeddings, neg_style_embeddings = scr(
-            F.interpolate(img_print2write, size=(96, 96), mode='bilinear', align_corners=False),
-            data['target'],
-            data['negative'],
-            nce_layers='0,1,2,3')
-        sc_loss = scr.calculate_nce_loss(
-            sample_s=sample_style_embeddings,
-            pos_s=pos_style_embeddings,
-            neg_s=neg_style_embeddings)
+    set_requires_grad([content_encoder, style_encoder, decoder], True)
+    set_requires_grad([discriminator], False)
+    optimizer_G.zero_grad()
+    accelerator.backward(loss_G)
+    optimizer_G.step()
 
-        loss_D = criterionD(real_out, True) + criterionD(fake_out, False)
-        loss_G = criterionD(fake_out, True) + scr_coef * sc_loss
+    cont, residulte_features = content_encoder(data['content'])
+    style_emb, style_fc, residual_features_style = style_encoder(data['style'])
+    img_print2write = decoder(cont, residulte_features, style_emb, style_fc, residual_features_style)
+    fake_out = discriminator(img_print2write)
+    real_out = discriminator(data['target'])
+    loss_D = criterionD(real_out, True) + criterionD(fake_out, False)
 
-        accelerator.backward(loss_D)
-        optimizer_D.step()
+    set_requires_grad([content_encoder, style_encoder, decoder], False)
+    set_requires_grad([discriminator], True)
+    optimizer_D.zero_grad()
+    accelerator.backward(loss_D)
+    optimizer_D.step()
 
-        accelerator.backward(loss_G)
-        optimizer_G.step()
+    pbar.update(1)
+    pbar.set_postfix(str=f"loss_G: {loss_G}, loss_D: {loss_D}")
 
-    save_package(
-        img_dict={f"generated{str(i)}": img_print2write[i, :, :, :] for i in range(len(img_print2write))},
-        model_dict={
-            "content_encoder": content_encoder.state_dict(),
-            "style_encoder": style_encoder.state_dict(),
-            "decoder": decoder.state_dict(),
-            "discriminator": discriminator.state_dict()
-        },
-        path=save_path,
-        prefix=os.path.basename(__file__).replace(".py", ""),
-    )
+    if c % 100 == 0:
+        save_package(
+            img_dict={"generated_img": (data['target'][0, :, :, :], img_print2write[0, :, :, :])},
+            model_dict={
+                "content_encoder": content_encoder.state_dict(),
+                "style_encoder": style_encoder.state_dict(),
+                "decoder": decoder.state_dict(),
+                "discriminator": discriminator.state_dict()
+            },
+            path=save_path,
+            prefix=os.path.basename(__file__).replace(".py", ""),
+        )
+    c += 1
