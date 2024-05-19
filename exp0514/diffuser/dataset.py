0ea63712597821fe8047570cf25d4ab2aafdf8e8
Leo <leo.z@kakaoent.com>
2024-05-19 22:16:21 +0000

    exp0514 on

--- exp0514/diffuser/dataset.py
+++ exp0514/diffuser/dataset.py
@@ -39,15 +39,25 @@ def get_nonorm_transform():
 
 
 class FontDataset(Dataset):
-    def __init__(self, path, num_neg=4, scr=False):
+    def __init__(self, path):
         super().__init__()
         self.path = path
+        self.resolution = 96 # default
         self.num_neg = num_neg
-        self.target_images = [path+"pngs/"+f for f in os.listdir(path+"pngs/") if ".png" in f]
-        self.all_korean_letters = pd.read_parquet(path+"all_korean.parquet")
-        self.scr = scr
-        if self.scr:
-            self.num_neg = num_neg
+        self.letter_mapper_a = pd.read_pickle(f"{path}/pickle/letter_mapper_a.pickle")
+        self.letter_mapper_b = pd.read_pickle(f"{path}/pickle/letter_mapper_b.pickle")
+        self.font_mapper = pd.read_pickle(f"{path}/pickle/font_mapper.pickle")
+        self.letter_mapper_ab = self.letter_mapper_a.similar + self.letter_mapper_b.similar
+        self.fonts = self.font_mapper.index
+        
+        self.transform = transforms.Compose([
+            transforms.Resize((self.resolution, self.resolution)),
+            transforms.ToTensor(),
+            transforms.Normalize([0.5], [0.5])
+        ])
+
+        self.ak = self.get_all_korean()
+        self.onthot = {k:self.korean2label(k) for k in self.ak}        
         
         self.transforms = get_normal_transform()
         self.nonorm_transforms = get_nonorm_transform()
@@ -56,9 +66,11 @@ class FontDataset(Dataset):
         target_image_path = self.target_images[index]
         target_image_name = target_image_path.split('/')[-1]
         style, content = target_image_name.split(".png")[0].split("__")
+        content_encoding = torch.zeros([68,16,16])
+        content_encoding[np.where(self.onehot[content])[0],:,:] = 1
         
         # Read content image
-        content_image_path = f"{self.path}pngs/gulim__{content}.png"
+        content_image_path = f"{self.path}gulim__{content}.png"
         content_image = Image.open(content_image_path).convert('RGB')
 
         # Random sample used for style image
@@ -79,7 +91,8 @@ class FontDataset(Dataset):
             "style_image": style_image,
             "target_image": target_image,
             "target_image_path": target_image_path,
-            "nonorm_target_image": nonorm_target_image}
+            "nonorm_target_image": nonorm_target_image,
+            "content_encoding": content_encoding}
         
         if self.scr:
             # Get neg image from the different style of the same content
@@ -102,3 +115,51 @@ class FontDataset(Dataset):
 
     def __len__(self):
         return len(self.target_images)
+    
+    def korean2onehot(self, letter):
+        ch1 = (ord(letter) - ord('가'))//588
+        ch2 = ((ord(letter) - ord('가')) - (588*ch1)) // 28
+        ch3 = (ord(letter) - ord('가')) - (588*ch1) - 28*ch2
+        return torch.from_numpy(np.concatenate([
+            np.eye(19)[ch1],
+            np.eye(21)[ch2],
+            np.eye(28)[ch3],
+        ]))
+    
+    def get_all_korean(self):
+
+        def nextKorLetterFrom(letter):
+            lastLetterInt = 15572643
+            if not letter:
+                return '가'
+            a = letter
+            b = a.encode('utf8')
+            c = int(b.hex(), 16)
+
+            if c == lastLetterInt:
+                return False
+
+            d = hex(c + 1)
+            e = bytearray.fromhex(d[2:])
+
+            flag = True
+            while flag:
+                try:
+                    r = e.decode('utf-8')
+                    flag = False
+                except UnicodeDecodeError:
+                    c = c+1
+                    d = hex(c)
+                    e = bytearray.fromhex(d[2:])
+            return e.decode()
+
+        returns = []
+        flag = True
+        k = ''
+        while flag:
+            k = nextKorLetterFrom(k)
+            if k is False:
+                flag = False
+            else:
+                returns.append(k)
+        return returns
